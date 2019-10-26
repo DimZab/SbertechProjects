795206c7a37365f0530a47f190280bc5d5214df9
Dmitrij <demzamajrulit@yandex.ru>
2019-10-26 12:56:12 +0300

    Lesson01

---
+++ src/com/sbt/javaschoolrnd/d1zab/lesson02/MyFirstJavaClass.java
@@ -0,0 +1,12 @@
+package com.sbt.javaschoolrnd.d1zab.lesson02;
+
+import com.sbt.javaschoolrnd.d1zab.lesson01.mysubpackage.MySecondJavaClass;
+
+public class MyFirstJavaClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!!!");
+
+    }
+}
