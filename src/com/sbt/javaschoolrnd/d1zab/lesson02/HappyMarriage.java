27ac2a5fb26569c3baf3092e42d66a8d871e593e
Dmitrij <demzamajrulit@yandex.ru>
2019-10-30 19:45:42 +0300

    Lesson02

---
+++ src/com/sbt/javaschoolrnd/d1zab/lesson02/HappyMarriage.java
@@ -0,0 +1,31 @@
+package com.sbt.javaschoolrnd.d1zab.lesson02;
+
+public class HappyMarriage {
+    public static void main(String[] args) {
+
+        // Adding 3 males and 3 females and trying them to marry
+
+        Person Andrei = new Person(true, "Andrei");
+        Person Denis = new Person(true, "Denis");
+        Person Ivan = new Person (true, "Ivan");
+
+        Person Anna = new Person (false, "Anna");
+        Person Diana = new Person (false, "Diana");
+        Person Maria = new Person (false, "Maria");
+
+
+        Andrei.marry(Anna);
+        Andrei.marry(Denis);
+
+        Denis.marry(Diana);
+        Anna.marry(Diana);
+
+
+        Andrei.marry(Diana);
+
+        System.out.println(Andrei.seeStats());
+        System.out.println(Anna.seeStats());
+
+
+    }
+}
