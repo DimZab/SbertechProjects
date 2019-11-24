ba605d9810a5cea381492d614740e2c9ea3045ef
Dmitrij <demzamajrulit@yandex.ru>
2019-11-24 19:49:09 +0300

    Assignments 6 and 7 are in progress.

--- src/com/sbt/javaschoolrnd/d1zab/lesson04/wildcard/Wildcards.java
+++ src/com/sbt/javaschoolrnd/d1zab/lesson04/wildcard/Wildcards.java
@@ -2,6 +2,7 @@ package com.sbt.javaschoolrnd.d1zab.lesson04.wildcard;
 
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Wildcards {
@@ -9,17 +10,71 @@ public class Wildcards {
 
 
         List<Vehicle> vehicles = new ArrayList<Vehicle>();
+        List<Auto> autos = new ArrayList<>();
         List<Plane> planes = new ArrayList<>();
 
+
         planes.add(new Plane("Boeing"));
-
-
         getVehicleName(planes);
 
-
-        List<Auto> autos = new ArrayList<>();
         autos.add(new Auto("Mercedes"));
         getVehicleName(autos);
+
+
+        planes.add(new Plane("Stealth"));
+        autos.add(new Auto("bmw"));
+
+
+        Comparator<Vehicle> vehicleComparator = new Comparator<Vehicle>() {
+
+            @Override
+            public int compare(Vehicle o1, Vehicle o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+
+
+        Comparator<Object> objectComparator = new Comparator<Object>() {
+
+            @Override
+            public int compare(Object o1, Object o2) {
+                return Integer.compare(o1.hashCode(), o2.hashCode());
+            }
+        };
+
+        compareWithPecs(planes, vehicleComparator);
+        compareWithPecs(planes, objectComparator);
+
+
+        compareWithPecs(planes, new Comparator<Plane>() {
+
+            @Override
+            public int compare(Plane o1, Plane o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+    }
+
+    static <T extends Vehicle> Vehicle compareWithPecs(List<? extends T> vehicles, Comparator<? super T> comparator) {
+        if (vehicles.isEmpty()) {
+            return null;
+        }
+
+        T max = vehicles.get(0);
+
+        for (T vehicle : vehicles) {
+            if (comparator.compare(max, vehicle) < 0) {
+
+//                The return value(not type, the type is int) tells the caller(the thing sorting the data):
+//                - 1 : o1 < o2  ;
+//                0   : o1 == o2 ;
+//                + 1 : o1 > o2
+
+                max = vehicle;
+            }
+        }
+        // System.out.println(max.getName());
+        return max;
     }
 
 
