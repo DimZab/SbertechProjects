12895360d6b0ce1165061c9e0302c88908b97cd6
Dmitrij <demzamajrulit@yandex.ru>
2019-11-30 23:43:39 +0300

    Rearranged checking methods. Fixed problem with Pin checking.

---
+++ src/com/sbt/javaschoolrnd/d1zab/lesson05/Terminal/Main.java
@@ -0,0 +1,13 @@
+package com.sbt.javaschoolrnd.d1zab.lesson05.Terminal;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TerminalUI ui = new TerminalUI();
+
+        ui.start();
+    }
+}
---
+++ src/com/sbt/javaschoolrnd/d1zab/lesson05/Terminal/TerminalImpl.java
@@ -0,0 +1,128 @@
+package com.sbt.javaschoolrnd.d1zab.lesson05.Terminal;
+
+import java.io.IOException;
+
+public class TerminalImpl implements Terminal {
+
+    private final TerminalServer server;
+    private final CardAndPinValidator cardAndPinValidator;
+
+    private final LockHelper lockHelper = new LockHelper();
+
+    private int pinErrorCount = 0;
+    private int cardNumberErrorCount = 0;
+
+    public TerminalImpl(TerminalServer server, CardAndPinValidator cardAndPinValidator) {
+        this.server = server;
+        this.cardAndPinValidator = cardAndPinValidator;
+    }
+
+    public boolean checkCardNumber(String cardNumber) {
+
+        if (lockHelper.isLocked())
+            throw new AccountIsLockedException((int) (Math.ceil(lockHelper.getCoutdown() / 1000.0)));
+
+        if (cardAndPinValidator.validateCardNumber(cardNumber)) {
+            cardNumberErrorCount = 0;
+            return true;
+        }
+        return false;
+
+        // We can lock terminal if entered card Number is invalid
+
+//        cardNumberErrorCount++;
+//        if (cardNumberErrorCount == 2) {
+//            lockHelper.lock();
+//            cardNumberErrorCount = 0;
+//        }
+//
+//        return false;
+    }
+
+
+    public boolean checkPin(String cardNumber, String pin) {
+
+        if (lockHelper.isLocked())
+            throw new AccountIsLockedException((int) (Math.ceil(lockHelper.getCoutdown() / 1000.0)));
+
+        if (cardAndPinValidator.validateCardAndPin(cardNumber, pin)) {
+            pinErrorCount = 0;
+            return true;
+        }
+
+        pinErrorCount++;
+        if (pinErrorCount == 2) {
+            lockHelper.lock();
+            pinErrorCount = 0;
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public void getMoney(String cardNumber, long amount) throws IOException {
+        if (!cardAndPinValidator.isAuthorized)
+            throw new SecurityException("The user is not authorized. Please, enter the PIN code");
+        if (lockHelper.isLocked())
+            throw new AccountIsLockedException((int) (Math.ceil(lockHelper.getCoutdown() / 1000.0)));
+        if (amount % 100 != 0)
+            throw new IllegalArgumentException("Amount must be a multiple of 100");
+
+        server.getMoney(cardNumber, amount);
+    }
+
+    @Override
+    public void putMoney(String cardNumber, long amount) throws IOException {
+        if (!cardAndPinValidator.isAuthorized)
+            throw new SecurityException("The user is not authorized. Please, enter the PIN code");
+        if (lockHelper.isLocked())
+            throw new AccountIsLockedException((int) (Math.ceil(lockHelper.getCoutdown() / 1000.0)));
+        if (amount % 100 != 0)
+            throw new IllegalArgumentException("Amount must be a multiple of 100");
+
+        server.putMoney(cardNumber, amount);
+    }
+
+    @Override
+    public long checkBalance(String cardNumber) throws IOException {
+        if (!cardAndPinValidator.isAuthorized)
+            throw new SecurityException("The user is not authorized. Please, enter the PIN code");
+        if (lockHelper.isLocked())
+            throw new AccountIsLockedException((int) (Math.ceil(lockHelper.getCoutdown() / 1000.0)));
+
+        return server.checkBalance(cardNumber);
+    }
+
+    /**
+     * Helper class tracking account lockout time
+     */
+
+    private static class LockHelper {
+        private static final long lockInterval = 5000;
+        long lockTime = -1;
+
+        boolean isLocked() {
+            if (lockTime == -1) return false; //Was not blocked
+            if (lockTime + lockInterval > System.currentTimeMillis()) {
+                return true; //Interval is not yet completed
+            }
+            //Interval is completed
+            lockTime = -1;
+            return false;
+        }
+
+        void lock() {
+            if (lockTime != -1) return; //Already blocked
+            lockTime = System.currentTimeMillis();
+        }
+
+        //Returns the time in milliseconds before unlocking
+        long getCoutdown() {
+            if (lockTime != -1)
+                return lockTime + lockInterval - System.currentTimeMillis();
+            return 0;
+        }
+    }
+}
+
